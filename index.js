a0572c1ce8c81603140fcadee50cce93f23a841c
Hazel29G <89797957+Hazel29G@users.noreply.github.com>
2021-09-06 19:10:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-h1 {
- font-family:sans-serif;
-  color:blue;
-}
 
-  headertwo{
-  color:pink;
+function getIMC(){
+  var peso=parseFloat(document.getElementById(´Peso´).value);
+ var altura=parseFloat(document.getElementById(´Altura´).value);
+  var imc=peso/(altura*altura);
+  document.getElementbyId("imc").innerHTML=imc;
 }
